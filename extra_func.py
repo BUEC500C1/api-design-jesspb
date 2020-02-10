5f188f1ea83d4f26111d904b1b392a26f6daf598
jesspb <jesspb@users.noreply.github.com>
2020-02-09 21:56:32 -0500

    add functions

--- extra_func.py
+++ extra_func.py
@@ -1,9 +1,20 @@
 import csv
-import sys
 import requests
-import pytemperature
 
-def getAirportFromCode(code)
+def getCityFromCode(code):
     with open("airports/airports_csv.csv", 'r') as fileCSV:
         csvreader = csv.reader(fileCSV)
-        
+        for val in csvreader:
+            if val[1] == ident:
+                return val[10]
+    return ""
+
+
+#  finds airport name given an airport name
+def getCityFroAirport(airport):
+    with open("airports/airports_csv.csv", 'r') as filrCSV:
+        csvreader = csv.reader(csvfile)
+        for val in csvreader:
+            if val[3] == airport_name:
+                return val[10]
+    return ""
--- weather_api.py
+++ weather_api.py
@@ -4,6 +4,36 @@ from flask import jsonify
 import requests
 from flask import request
 from extra_func import getAirportFromCode
+from config import api_key
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
+def call_api(city):
+    # openweathermap api call
+    url = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key
+
+    response = requests.get(url).json()
+
+    if (response["cod"] != "404") & (response["cod"] != "400"):
+
+        weath = response['main']
+        desc = response['weather'][0]['description']
+        temp = weath['temp']-273.13
+        feels_like = weath['feels_like']-273.13
+        humidity = weath['humidity']
+
+        weather = [
+
+            {
+            'Descripton': desc,
+            'Temperature (C)': temp,
+            'Feels Like (C)': feels_like,
+            'Humidity': humidity}
+        ]
+        return weather
+
+    else:
+        print("Error: Could not find city")
+        return -1
